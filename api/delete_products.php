1f005d060766e3d05866e6177b1841ee3decb4cd
sauzuniga <saulzuniga606@gmail.com>
2023-11-22 20:15:38 -0600

    se agrega la fución de eliminar y actualizar

---
+++ api/delete_products.php
@@ -0,0 +1,21 @@
+<?php
+	include("../config/connection.php");
+	$response=new stdClass();
+    $codpro=$_POST['codpro'];
+    $sql="select rutimapro from producto where codpro=$codpro";
+	$result=mysqli_query($con,$sql);
+	$row=mysqli_fetch_array($result);
+	$rutimapro=$row['rutimapro'];
+	$sql="delete from producto where codpro=$codpro";
+	$result=mysqli_query($con,$sql);
+    
+    if ($result) {
+        $response->state=true;
+        unlink("../../ecommerce/assets/".$rutimapro);
+    }else{
+        $response->state=false;
+        $response->detail="No se puede eliminar el producto";
+    }
+
+echo json_encode($response);
+    
--- productos.php
+++ productos.php
@@ -9,6 +9,7 @@ include('config/connection.php');
     <title>Productos</title>
     <link rel="stylesheet" type="text/css" href="css/index.css">
     <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
     <div class="modal" id="modal-producto" style="display: none;">
@@ -42,6 +43,41 @@ include('config/connection.php');
                 <button onclick="save_product()">Guardar</button>
         </div>
     </div>
+    <div class="modal" id="modal-producto-edit" style="display: none;">
+		<div class="body-modal">
+			<button class="btn-close" onclick="hide_modal('modal-producto-edit')"><i class="fa fa-times" aria-hidden="true"></i></button>
+			<h3>Editar producto</h3>
+			<div class="div-flex">
+				<label>Código</label>
+				<input type="text" id="codigo-e" disabled>
+			</div>
+			<div class="div-flex">
+				<label>Nombre</label>
+				<input type="text" id="nombre-e">
+			</div>
+			<div class="div-flex">
+				<label>Descripción</label>
+				<input type="text" id="descripcion-e">
+			</div>
+			<div class="div-flex">
+				<label>Precio</label>
+				<input type="number" id="precio-e">
+			</div>
+			<input type="text" id="rutimapro-aux" style="display: none;">
+			<div class="div-flex">
+				<label>Estado</label>
+				<select id="estado-e">
+					<option value="1">Activo</option>
+					<option value="0">Inactivo</option>
+				</select>
+			</div>
+			<img id="rutimapro" src="" style="width: 200px;margin: 5px auto;">
+			<div class="div-flex">
+				<input type="file" id="imagen-e">
+			</div>
+			<button onclick="update_product()">Actualizar</button>
+		</div>
+	</div>
     <div class="main-container">
         <div class="body-nav-bar">
             <img src="assets/web/niche.png" alt="">
@@ -80,8 +116,8 @@ include('config/connection.php');
                     <td>'.$row['prepro'].'</td>
                     <td class="td-option">
                     <div class="div-flex div-td-button">
-                        <button><i class="fa fa-pencil" aria-hidden="true"></i></button>
-                        <button><i class="fa fa-trash" aria-hidden="true"></i></button>
+                        <button onclick="edit_products('.$row['codpro'].')"><i class="fa fa-pencil" aria-hidden="true"></i></button>
+                        <button onclick="delete_products('.$row['codpro'].')"><i class="fa fa-trash" aria-hidden="true"></i></button>
                     </div>
                     </td>
                     </tr>
@@ -132,6 +168,75 @@ include('config/connection.php');
             }
             request.send(fd)
         }
+        function delete_products(codpro) {
+    var c = confirm("¿Estás seguro de eliminar el producto de código " + codpro + "?");
+    if (c) {
+        let fd = new FormData();
+        fd.append('codpro', codpro);  // Cambiado a 'codpro' en lugar de 'codigo'
+        let request = new XMLHttpRequest();
+        request.open('POST', 'api/delete_products.php', true);
+        request.onload = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                let response = JSON.parse(request.responseText);
+                console.log(response);
+                if (response.state) {
+                    alert("Producto eliminado");
+                    window.location.reload();
+                } else {
+                    alert(response.detail);
+                }
+            }
+        }
+        request.send(fd);
+    }
+}
+function edit_products(codpro){
+			let fd=new FormData();
+			fd.append('codpro',codpro);
+			let request=new XMLHttpRequest();
+			request.open('POST','api/get_product.php',true);
+			request.onload=function(){
+				if (request.readyState==4 && request.status==200) {
+					let response=JSON.parse(request.responseText);
+					console.log(response);
+					document.getElementById("codigo-e").value=codpro;
+					document.getElementById("nombre-e").value=response.product.nompro;
+					document.getElementById("descripcion-e").value=response.product.despro;
+					document.getElementById("precio-e").value=response.product.prepro;
+					document.getElementById("estado-e").value=response.product.estado;
+					document.getElementById("rutimapro").src="../ecommerce/assets/"+response.product.rutimapro;
+					document.getElementById("rutimapro-aux").value=response.product.rutimapro;
+					show_modal('modal-producto-edit');
+					//imagen-e
+				}
+			}
+			request.send(fd);
+		}
+        function update_product(){
+			let fd=new FormData();
+			fd.append('codigo',document.getElementById('codigo-e').value);
+			fd.append('nombre',document.getElementById('nombre-e').value);
+			fd.append('descripcion',document.getElementById('descripcion-e').value);
+			fd.append('precio',document.getElementById('precio-e').value);
+			fd.append('estado',document.getElementById('estado-e').value);
+			fd.append('imagen',document.getElementById('imagen-e').files[0]);
+			fd.append('rutimapro',document.getElementById("rutimapro-aux").value);
+			let request=new XMLHttpRequest();
+			request.open('POST','api/update_products.php',true);
+			request.onload=function(){
+				if (request.readyState==4 && request.status==200) {
+					let response=JSON.parse(request.responseText);
+					console.log(response);
+					if (response.state) {
+						alert("El producto se ha actualizado");
+						window.location.reload();
+					}else{
+						alert(response.detail);
+					}
+				}
+			}
+			request.send(fd);
+		}
     </script>
    
     
